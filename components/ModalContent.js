ab4a0bf42a28642da4cb9e7d4491f639ed325d5f
Samantha Lai <samanthalai@Samanthas-MacBook-Pro-2.local>
2021-01-10 14:46:06 -0800

    form updates state using useReducer

--- components/ModalContent.js
+++ components/ModalContent.js
@@ -1,18 +1,75 @@
-// create a form for edit (scan barcode) and add item wothout any db reads/writes
-// get the info from the form into correct ofrmat to write to db
-// style the page
-// merge work into master and branch again to incorporate sam work
-// make db call 
-import { useForm } from "react-hook-form";
+import React, { useState, useReducer } from 'react';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 
-export default function ModalContent(close) {
-    const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = data => {
-        console.log(data);
-        // write to firebase
+
+function formReducer(state, action) {
+    switch (action.type) { // actions have a type by convention, value to hold 
+        case 'editItemName': {
+            return {
+                ...state,
+                itemName: action.value
+            }
+        } 
+        case 'editItemBarcode': {
+            return {
+                ...state,
+                barcode: action.value
+            }
+        }         
+        case 'editItemCount': {
+            return {
+                ...state,
+                count: action.value
+            }
+        }            
+        case 'editItemPackSize': {
+            return {
+                ...state,
+                packSize: action.value
+            }
+        }  
+        case 'editCategories': {
+            return {
+                ...state,
+                categories: action.value
+            }
+        }       
+        case 'editItemLowStock': {
+            return {
+                ...state,
+                lowStock: action.value
+            }
+        }    
+        default:
+            break;
     }
+    return state
+}
 
-    console.log(watch("example")); // watch input value by passing the name of it
+const initialState = {
+    itemName: "",
+    barcode: "",
+    count: "",
+    packSize: "",
+    categories: "",
+    lowStock: "",
+}
+
+const categoryOptions = [
+    { label: 'Grains', value: 1},
+    { label: 'Protein', value: 2},
+    { label: 'Snacks', value: 3},
+    { label: 'Frozen', value: 4},
+
+]
+
+
+export default function ModalContent() {
+    const [ state, dispatch ] = useReducer(formReducer, initialState)
+
+    function handleSubmit() {
+        console.log(state)
+    }
 
     return (
         <div className="modal-wrapper">
@@ -20,31 +77,46 @@ export default function ModalContent(close) {
                 <div className="modal-body">
                 </div>
                 <div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className='p-1'>
-                            <label className="p-2">Item Name</label>
-                            <input type="text" name="itemName" required ref={register}/>
-                        </div>
-                        <div className="p-1">
-                            <label className="p-2">Item Barcode</label>
-                            <input type="text" name="itemBarcode" required ref={register}/>
-                        </div>
-                        <div className='p-1'> 
-                            <label className="p-2">Stock Count</label>
-                            <input type="number" min="0" name="count" required ref={register}/>
-                        </div>
-                        <div className='p-1'>
-                            <label className="p-2">Pack Size</label>
-                            <input type="number" min="0" name="packSize" required ref={register}/>
-                        </div>
-                        <div className='p-1'>
-                            <label className="p-2">Low Stock Threshold</label>
-                            <input type="number" min="0" name="lowStock" required ref={register}/>
-                        </div>
-                        <div className='p-3' >
-                            <button className="bg-gray-300 p-2 rounded-md" type="submit">Submit</button>
-                        </div>
-                    </form> 
+                    <div className='p-1'>
+                        <label className="p-2 font-bold">Item Name</label>
+                        <input type="text" name="itemName" required onBlur={(e) => {
+                            dispatch({type: 'editItemName', value: e.currentTarget.value})
+                        }}/>
+                    </div>
+                    <div className="p-1">
+                        <label className="p-2 font-bold">Item Barcode</label>
+                        <input type="text" name="itemBarcode" required onBlur={(e) => {
+                            dispatch({type: 'editItemBarcode', value: e.currentTarget.value})
+                        }}/>
+                    </div>
+                    <div className='p-1'> 
+                        <label className="p-2 font-bold">Stock Count</label>
+                        <input type="number" min="0" name="count" required onBlur={(e) => {
+                            dispatch({type: 'editItemCount', value: e.currentTarget.value})
+                        }}/>
+                    </div>
+                    <div className='p-1'>
+                        <label className="p-2 font-bold">Pack Size</label>
+                        <input type="number" min="0" name="packSize" required onBlur={(e) => {
+                            dispatch({type: 'editItemPackSize', value: e.currentTarget.value})
+                        }}/>
+                    </div>                        
+                    <div className='p-1'>
+                        <ReactMultiSelectCheckboxes options={categoryOptions} placeholderButtonLabel="Select Categories" onChange={(selectedOptions)=>{
+                            dispatch({type: 'editCategories', value: selectedOptions})
+
+                        }}/>
+                    </div>
+                    <div className='p-1'>
+                        {/* {not required} */}
+                        <label className="p-2 font-bold">Low Stock Threshold</label> 
+                        <input type="number" min="0" name="lowStock" onBlur={(e) => {
+                            dispatch({type: 'editItemLowStock', value: e.currentTarget.value})
+                        }}/>
+                    </div>
+                    <div className='p-3' >
+                        <button className="bg-gray-300 p-2 rounded-md" type="submit" onClick={handleSubmit}>Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
