27a0c312fe4879fc1016dad43733e6f6317872d3
Janise Liang <jliang5067@gmail.com>
2022-03-05 14:53:30 -0800

    max order quantity validation

--- components/orderForm/OrderDetails.js
+++ components/orderForm/OrderDetails.js
@@ -7,7 +7,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function OrderDetails() {
   const cartDispatch = useContext(DispatchCartContext)
-  const cartState = useContext(StateCartContext)
+  const { cart } = useContext(StateCartContext)
   const { data, error } = useSWR('/api/inventory/GetAllItems', fetcher)
   
   if (error) return <div>failed to load</div>
@@ -15,6 +15,9 @@ export default function OrderDetails() {
 
   // Generate inputs
   const orderInputs = Object.keys(data).map((key, _value) => {
+    const maxQuantity = data[key].maxOrderSize
+    var invalid_quantity = cart[key] && cart[key].quantity > maxQuantity
+
     return (
       <div key={key} className="mb-4">
         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for={key}>
@@ -24,12 +27,15 @@ export default function OrderDetails() {
           id={key}
           type="number" 
           autoComplete="off"
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className={"appearance-none block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        + (invalid_quantity ? " border-red-600 focus:border-red-600 border-2" : " border")}
+          value={cart[key] ? cart[key].quantity : ""}
           onChange={(e) => {
             cartDispatch({ type: 'UPDATE_CART', payload: {item: data[key], quantity: e.target.value } });
             }
           }
           />
+          {invalid_quantity ? <div className='text-red-600'>Quantity must be less than {maxQuantity}</div> : ""}
       </div>
     )
   })
