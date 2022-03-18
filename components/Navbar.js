c3d43cc2972670f5a10d221b174b699c94397ca1
Janise Liang <jliang5067@gmail.com>
2022-03-17 22:22:48 -0700

    fixing some warnings; making sure footer is actually at the bottom of page & shrinking footer padding a tiny bit

--- components/Navbar.js
+++ components/Navbar.js
@@ -79,7 +79,7 @@ export default function Navbar() {
       <div className='md:order-2'>
         {!user ? <a className="bg-gray-100 text-gray-700 rounded px-3 py-1 " href="/signin">Sign In</a> :
           <div className="flex flex-col float-left md: order-2">
-            <button className="focus:outline-none w-8 h-8 rounded-full bg-gray-100 text-gray-700 font-semibold" onClick={toggleUserDropdown}>
+            <button className="focus:outline-none w-8 h-8 rounded-full bg-gray-100 text-gray-700 font-semibold truncate" onClick={toggleUserDropdown}>
               {name.split(" ").map((n)=>n[0]).join("") /* circle with initials */}
             </button>
             <div className={(showUserInfo ? "" : "hidden ") + 
@@ -102,9 +102,9 @@ export default function Navbar() {
 
         {/* drop-down menu button for small screens */}
         <button type="button" onClick={toggleShowTabs}
-          class="inline-flex items-center p-1 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none" aria-controls="mobile-menu-2" aria-expanded="false">
-          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          className="inline-flex items-center p-1 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none" aria-controls="mobile-menu-2" aria-expanded="false">
+          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+          <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </button>
       </div>
 
