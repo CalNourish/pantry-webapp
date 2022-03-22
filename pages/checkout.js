b7f133635e0fc62492794cc0d4e4d2b86c010683
Janise Liang <jliang5067@gmail.com>
2022-03-22 01:29:12 -0700

    prettify increment/decrement for quantities

--- pages/checkout.js
+++ pages/checkout.js
@@ -6,11 +6,11 @@ import React from 'react';
 import cookie from 'js-cookie';
 
 /* TODO:
-  * hotkeys
-  * make buttons look nicer
-  * make increment/decrement look nicer
-  * small screen spacings
-  * item search? esp for things with no barcodes
+  x hotkeys
+  x make buttons look nicer
+  x make increment/decrement look nicer
+  o item search? esp for things with no barcodes
+  o small screen spacings
 */
 
 const fetcher = async (...args) => {
@@ -70,6 +70,7 @@ class Cart extends React.Component {
       var itemData = items.get(barcode)
       itemData[1] += quantity
       items.set(barcode, itemData)
+      document.getElementById(barcode+"-quantity").value = itemData[1]; // update the input field
     } else { /* otherwise, create new row in table */
       items.set(newItem.barcode, [newItem, quantity])
     }
@@ -90,6 +91,7 @@ class Cart extends React.Component {
     itemData[1] += 1;
     items.set(barcode, itemData);
     this.setState({items: items, itemsInCart: this.state.itemsInCart + 1})
+    document.getElementById(barcode+"-quantity").value = itemData[1]; // update the input field
   }
 
   downItemQuantity(barcode) {
@@ -111,6 +113,24 @@ class Cart extends React.Component {
     itemData[1] -= 1;
     items.set(barcode, itemData);
     this.setState({items: items, itemsInCart: this.state.itemsInCart - 1})
+    document.getElementById(barcode+"-quantity").value = itemData[1]; // update the input field
+  }
+
+  updateItemQuantity(barcode, newQuantity) {
+    let items = this.state.items;
+    let itemData = items.get(barcode);
+    if (!itemData) {
+      this.showError("Data corruption: please retry or refresh page", 20000)
+      return
+    }
+
+    newQuantity = parseInt(newQuantity)
+    if (!isNaN(newQuantity)) {
+      let deltaQuantity = newQuantity - itemData[1]; // amount increased by, to recalculate the total sum
+      itemData[1] = newQuantity;
+      items.set(barcode, itemData);
+      this.setState({items: items, itemsInCart: this.state.itemsInCart+deltaQuantity})
+    }
   }
 
   deleteItem(barcode) {
@@ -152,6 +172,10 @@ class Cart extends React.Component {
   submitCart = async (e) => {
     e.preventDefault();
     const token = cookie.get("firebaseToken")
+    // const allowed = await validateFunc(token)
+    // console.log(allowed)
+    // return;
+
     let reqbody = this.makeReq();
     this.showSuccess("Submitting cart...", 10000)
 
@@ -184,25 +208,27 @@ class Cart extends React.Component {
   displayCartRow(barcode, value) {
     return (
       <tr className="h-10" key={barcode}>
-        <td className="text-left">{value[0].itemName}</td>
+        <td className="text-left pr-10">{value[0].itemName}</td>
         <td className="">
-          <div className="float-left">{value[1]}</div>
-          <div className="float-right pl-5 pr-2">
-            <button className="font-bold text-xl" onClick={() => this.downItemQuantity(barcode)}>
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            </button>
-            <button className="font-bold text-xl pl-2" onClick={() => this.upItemQuantity(barcode)}>
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            </button>
+          {/* stepper */}
+          <div className="border border-solid border-gray-300 p-px w-32 h-8 flex flex-row float-left">
+            {/* minus */}
+            <button className="font-light p-1 bg-gray-300 w-8 h-full text-xl leading-3 focus:outline-none" onClick={() => this.downItemQuantity(barcode)}>–</button>
+            {/* input */}
+            <input id={barcode + "-quantity"} className="w-6 flex-grow mx-1 text-center focus:outline-none" autoComplete="off"
+              defaultValue={value[1]} onChange={e => this.updateItemQuantity(barcode, e.target.value)}/>
+            {/* plus */}
+            <button className="font-light p-1 bg-gray-300 w-8 h-full text-xl leading-3 focus:outline-none" onClick={() => this.upItemQuantity(barcode)}>+</button>
           </div>
-        </td>
-        <td className="" onClick={() => this.deleteItem(barcode)}>
-          <button>
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+          {/* Trash can symbol */}
+          <button className="float-right align-middle py-1">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" onClick={() => this.deleteItem(barcode)}>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
             </svg>
           </button>
         </td>
+        {/* <td className="" onClick={() => this.deleteItem(barcode)}>          
+        </td> */}
       </tr>
     )
   }
@@ -211,7 +237,8 @@ class Cart extends React.Component {
     const errorBanner = <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-3">{this.state.error}</div>;
     const successBanner = <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-3">{this.state.success}</div>;
 
-    const submitFunc = this.submitCart;
+    const cartSubmit = this.submitCart;
+    // const itemSubmit = this.itemFormSubmit;
 
     document.onkeydown = function(e){
       var barcode = document.getElementById("barcode");
@@ -225,8 +252,13 @@ class Cart extends React.Component {
         e.preventDefault();
         quantity.focus();
       }
-      else if (e.shiftKey && e.key == "Enter") {
-        submitFunc(e);
+      else if (e.key === "Enter" && e.shiftKey) {
+        e.preventDefault();
+        cartSubmit(e);
+      } else if (e.key === "Enter") {
+        e.preventDefault();
+        document.getElementById("add-item-btn").click();
+        // is there a better way to do this...
       }
     }
 
@@ -236,48 +268,56 @@ class Cart extends React.Component {
           <div className="flex h-full">
             {/* Left-hand column (Barcode and Quantity form) */}
             <div className="flex-none w-64">
-              <Sidebar className="py-5">
-              <form className="m-1" id="checkout-item-form" onSubmit={(e) => this.itemFormSubmit(e)}>
+              <Sidebar>
+              <form className="sticky top-0 py-4" id="checkout-item-form" onSubmit={(e) => this.itemFormSubmit(e)}>
                   <h1 className="text-3xl font-semibold mb-2">Checkout Item</h1>
                   <p className="mb-5">Please enter the amount, then scan the item to add it to the cart. Click "Check Out" to submit the cart.</p>
                   <div className="form-group" id="barcode-and-quantity">
                     <div className="col-xs-7 mb-4">
-                      <h1 className="text-2xl font-medium mb-2" autoFocus>Barcode</h1>
-                      <input className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="barcode" autoComplete="off" placeholder="scan item barcode"></input>
+                      <h1 className="text-2xl font-medium" autoFocus>Barcode</h1>
+                      <p className="text-gray-600 text-xs tracking-normal font-normal mb-2">
+                        (hotkey: Q)
+                      </p>
+                      <input className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="barcode" autoComplete="off" placeholder=""></input>
                     </div>
                     <div className="col-xs-8 mb-4">
-                      <h1 className="text-2xl font-medium mb-2">Quantity</h1>
+                      <h1 className="text-2xl font-medium">Quantity</h1>
+                      <p className="text-gray-600 text-xs tracking-normal font-normal mb-2">(hotkey: W)</p>
                       <input className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="quantity" autoComplete="off" placeholder="default: 1"></input>
                     </div>
                   </div>
-                  <button className="my-1 btn btn-pantry-blue w-full" type="submit">Add Item</button>
+                  <button className="my-1 btn btn-pantry-blue w-full uppercase tracking-wide text-xs font-semibold" id="add-item-btn" type="submit">Add Item (Enter)</button>
                 </form>
               </Sidebar>
             </div>
 
-            {/* Main body (Cart and Checkout) */}
-            <div className="grow p-5">
+            {/* Main body (Cart and Checkout Button) */}
+            <div className="p-5 container mx-auto">
               {this.state.error && errorBanner}
               {this.state.success && successBanner}
               <h1 className="text-3xl font-medium mb-2">Cart</h1>
-              <table className="w-full table-fixed my-5" id="mycart">
+              <table className="w-full my-5 table-fixed" id="mycart">
                 <thead>
                   <tr className="border-b-2">
                     <th className="w-auto text-left">Item</th>
-                    <th className="w-1/6 text-left">Quantity</th>
-                    <th className="w-5"></th>
+                    <th className="text-left w-48">
+                      <div className="w-32 text-center">Quantity</div>
+                    </th>
+                    {/* <th className="w-5 flex-grow-0"></th> */}
                   </tr>
                 </thead>
                 <tbody className="divide-y">
                   {Array.from( this.state.items ).map(([barcode, value]) => (this.displayCartRow(barcode, value)))}
                   <tr className="bg-gray-200 h-10 m-3" key="totals">
-                    <td className="text-lg font-medium text-right px-20">Total Items</td>
-                    <td className="">{this.state.itemsInCart}</td>
-                    <td></td>
+                    <td className="text-lg font-medium text-right pr-10">Total Items</td>
+                    <td>
+                      <div className="w-32 text-center font-medium">{this.state.itemsInCart}</div>
+                    </td>
+                    {/* <td></td> */}
                   </tr>
                 </tbody>
               </table>
-              <button className="btn my-1 btn-pantry-blue w-40 font-semibold" onClick={(e) => this.submitCart(e)}>Checkout</button>
+              <button className="btn my-1 btn-pantry-blue uppercase tracking-wide text-xs font-semibold" onClick={(e) => this.submitCart(e)}>Checkout (Shift+Enter)</button>
             </div>
           </div>
         </Layout>
