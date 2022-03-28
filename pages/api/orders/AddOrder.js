b48eff3a09001b20d1589dd7e45dbb2be1d00def
Jake Son <jakeson@berkeley.edu>
2022-03-27 19:58:46 -0700

    write after end

--- pages/api/orders/AddOrder.js
+++ pages/api/orders/AddOrder.js
@@ -2,7 +2,6 @@ import {validateFunc} from '../validate';
 import { google } from 'googleapis'; 
 import firebase from "firebase/app";
 import { resolveHref } from 'next/dist/next-server/lib/router/router';
-import UpdateGoogleSheets from './UpdateGoogleSheets';
 export const config = { // https://nextjs.org/docs/api-routes/api-middlewares
   api: {
     bodyParser: true,
@@ -10,13 +9,14 @@ export const config = { // https://nextjs.org/docs/api-routes/api-middlewares
 } 
 
 function requireParams(body, res) {
-    /* require elements: First name, last name, email address, address, calID, 
+    /* require elements: First name, last name, frequency, address, calID, 
     delivery_date, order_timestamp, items in the order (orderschema as of now)
     items are represented as an object with barcode as key and quantity (count) as value*/
 
-    if (!body.firstName || !body.lastName || !body.address ||
-        !body.emailAddress || !body.calID || !body.items || !body.deliveryDate) {
-          res.json({error: "missing firstName||lastName||EmailAddress||Address||calID||items||deliveryDate\
+    if (!body.firstName || !body.lastName || !body.address || !body.frequency ||
+        !body.dependents || !body.dietaryRestrictions || !body.additionalRequests || 
+        !body.calID || !body.items || !body.deliveryDate || !body.deliveryWindow) {
+          res.json({error: "missing firstName||lastName||frequency||Address||dependents||calID||items||deliveryDate\
              in request"}); 
             res.status(400);
             return false;
@@ -38,7 +38,6 @@ function updateInventory(items) { //updates inventory in firebase
       value.json().then((inventoryJson) => {
          const inventoryUpdates = {}
          for (let item in items) {
-  
            if (inventoryJson[item]['count'] >= items[item]) { //if we have enough in inventory for order
             inventoryUpdates['/inventory/' + item + "/count"] = firebase.database.ServerValue.increment(-1 * items[item]);
            }
@@ -59,36 +58,43 @@ function updateInventory(items) { //updates inventory in firebase
   })
 }
 
-function addOrder(firstName, lastName, address, emailAddress, calID, items, deliveryDate, deliveryWindow) {
+
+function addOrder(firstName, lastName, address, frequency, dependents, dietaryRestrictions, additionalRequests, calID, items, deliveryDate, deliveryWindow) {
     //add order data to google sheets and firebase
-    // I used this resource as a guide: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+    // I used this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
     //add order data to three google sheets (pantry, bag-packing, doordash ) and add order to firebase
   return new Promise((resolve, reject) => {
     const target = ['https://www.googleapis.com/auth/spreadsheets'];
     var jwt = new google.auth.JWT(
+      //process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
       process.env.GOOGLE_SHEETS_CLIENT_EMAIL_TEST,
       null,
+      //(process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
       (process.env.GOOGLE_SHEETS_PRIVATE_KEY_TEST || '').replace(/\\n/g, '\n'),
       target
     );
     const sheets = google.sheets({ version: 'v4', auth: jwt });
 
-    //generate orderID: random six digit value 
+    //generate orderID: random six digit value. We check later to make sure that the item doesn't exist
     const orderID = Math.random().toString().slice(2, 8);
 
-    const date = 1;
+    let d = new Date();
+    const currentDate = d.getDate();
     //Food pantry master data sheet for tracking calIDs 
-    //current schema idea is [Timestamp, CalID (encrypted), unique order id]
+    //current schema is [current date, CalID (encrypted), unique order id]
     const request1 = {
       spreadsheetId: process.env.SPREADSHEET_ID_TEST,//PANTRY_DATA_SPREADSHEET_ID, 
-      range: "Sheet1!A:C", //name of sheet will prob not be Sheet1
+      //spreadsheetId: process.env.PANTRY_DATA_SPREADSHEET_ID,
+      range: "Sheet1!A:C", 
+      //range: "Tech Testing!A:C",
       valueInputOption: "USER_ENTERED", 
       insertDataOption: "INSERT_ROWS",
       resource: {
-          range: "Sheet1!A:C",
+          "range": "Sheet1!A:C",
+          // "range": "Tech Testing!A:C",
           "majorDimension": "ROWS",
           "values": [
-          [date, calID, orderID] //each inner array is a row if we specify ROWS as majorDim
+          [currentDate, calID, orderID] //each inner array is a row if we specify ROWS as majorDim
           ] 
         } 
     }
@@ -102,22 +108,26 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
     for (let item in items) {
       totalItems += items[item];
     }
-    const numberOfBags = Math.ceil(totalItems / 10) 
+    const numberOfBags = Math.ceil(totalItems / 10);
 
 
-    /*bag packing sheet with [delivery date, delivery window (as string), first name, last initial, # of dependents, dietary restritions,
-    orderID, # of bags, items, frequency ] 
+    /*bag packing sheet with [confirmed date (delivery date), first name + last initial, delivery window, # of bags, frequency, # of dependents, dietary restrictions,
+   additional requests, order id, items] 
     frequency: one-time or recurring (once a week or every two weeks for now) */
     const request2 = {
       spreadsheetId: process.env.SPREADSHEET_ID_TEST,//BAG_PACKING_SPREADSHEET_ID,
-      range: "Sheet1!A:G",
+      //spreadsheetId: process.env.BAG_PACKING_SPREADSHEET_ID,
+      range: "Sheet1!A:J",
+      //range: "[Testing] Spring Delivery Packing Info!A:J",
       valueInputOption: "USER_ENTERED", 
       insertDataOption: "INSERT_ROWS",
       resource: {
-          range: "Sheet1!A:G",
+          range: "Sheet1!A:J",
+          //"range": "[Testing] Spring Delivery Packing Info!A:J",
           "majorDimension": "ROWS",
           "values": [
-          [deliveryDate, deliveryWindow, firstName, lastName.slice(0,1), orderID, numberOfBags, JSON.stringify(items)] 
+            [deliveryDate, firstName + " " + lastName.slice(0,1), deliveryWindow, numberOfBags, frequency, 
+          dependents, dietaryRestrictions, additionalRequests, orderID, JSON.stringify(items)]
           ] 
         } 
     }
@@ -129,14 +139,15 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
     //door dash sheet with [deliveryDate, delivery window start/end, first name, last NAME, address, item code (always F), # of bags (must be <= 3) ]
     const request3 = {
       spreadsheetId: process.env.SPREADSHEET_ID_TEST,//DOORDASH_SPREADSHEET_ID,
-      range: "Sheet1!A:F",
+      //spreadSheetId: process.env.DOORDASH_SPREADSHEET_ID,
+      range: "Sheet1!A:H",
       valueInputOption: "USER_ENTERED", 
       insertDataOption: "INSERT_ROWS",
       resource: {
-          range: "Sheet1!A:F",
+          range: "Sheet1!A:H",
           "majorDimension": "ROWS",
           "values": [
-          ["VENTURA-01", deliveryDate, deliveryWindow, firstName, lastName, address, totalItems] 
+          ["VENTURA-01", deliveryDate, deliveryWindow, firstName, lastName, address, "F", totalItems] 
           ] 
         } 
     }
@@ -146,7 +157,7 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
     });
 
 
-    /*add order to firebase. Is Used AddItem code
+    /*add order to firebase. I used AddItem code
     */
 
     let newOrder = {};
@@ -154,14 +165,14 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
     newOrder["status"] = "Not started";
     //newOrder["type"] = "Delivery";
     newOrder["deliveryDate"] = deliveryDate;
-    newOrder["deliveryWindow"] = "some time idk";deliveryWindow;
+    newOrder["deliveryWindow"] = deliveryWindow;
     newOrder["notes from guest"] = "I'm, like, allergic to peanuts :( ";
     newOrder["firstName"] = firstName;
     newOrder["lastInitial"] = lastName.slice(0, 1);
 
     firebase.auth().signInAnonymously()
             .then(() => {
-              console.log(orderID);
+              console.log("orderID", orderID);
               let itemRef = firebase.database().ref('/order/' + orderID);
               itemRef.once('value')
               .catch(function(error){
@@ -180,7 +191,7 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
               // otherwise the item doesn't exist and we can create it
               itemRef.update(newOrder)
               .catch(function(error) {
-                  console.log("Not ok7")
+                  console.log("Not ok7: ", error)
                   return resolve();
               })
               .then(() => {
@@ -195,7 +206,7 @@ function addOrder(firstName, lastName, address, emailAddress, calID, items, deli
   })
 } 
   
-export default async function(req,res) {   
+export default async function(req, res) {   
     // verify this request is legit
     const token = req.headers.authorization
     console.log("TOKEN: ", token)
@@ -214,8 +225,9 @@ export default async function(req,res) {
     firebase.auth().signInAnonymously()
     .then(() => {
       updateInventory(body.items).then((success) => {
-        addOrder(body.firstName, body.lastName, body.address, body.emailAddress, 
-          body.calID, body.items, body.deliveryDate).then(() => {
+        addOrder(body.firstName, body.lastName, body.address, body.frequency, body.dependents, 
+          body.dietaryRestrictions, body.additionalRequests,
+          body.calID, body.items, body.deliveryDate, body.deliveryWindow).then(() => {
             console.log("Added to google sheets");
           })
           .catch((error) => {
