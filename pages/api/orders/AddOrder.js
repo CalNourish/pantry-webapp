87b428307393fda3e26571c69f3d5e88d19adb67
Janise Liang <jliang5067@gmail.com>
2022-04-14 22:45:01 -0700

    move env variables to top of file; using a selector to choose test or real sheets

--- pages/api/orders/AddOrder.js
+++ pages/api/orders/AddOrder.js
@@ -3,6 +3,15 @@ import { google } from 'googleapis';
 import firebase from "firebase/app";
 import { server } from '../../_app.js'
 import { resolveHref } from 'next/dist/next-server/lib/router/router';
+
+const test = true;
+const client_email = test ? process.env.GOOGLE_SHEETS_CLIENT_EMAIL_TEST : process.env.GOOGLE_SHEETS_CLIENT_EMAIL;
+const private_key = test? process.env.GOOGLE_SHEETS_PRIVATE_KEY_TEST : process.env.GOOGLE_SHEETS_PRIVATE_KEY;
+
+const pantry_sheet = test ? process.env.SPREADSHEET_ID_TEST : process.env.PANTRY_DATA_SPREADSHEET_ID;
+const doordash_sheet = test ? process.env.SPREADSHEET_ID_TEST : process.env.DOORDASH_SPREADSHEET_ID;
+const bag_packing_sheet = test ? process.env.SPREADSHEET_ID_TEST : process.env.BAG_PACKING_SPREADSHEET_ID;
+
 export const config = { // https://nextjs.org/docs/api-routes/api-middlewares
   api: {
     bodyParser: true,
@@ -34,9 +43,8 @@ function requireParams(body, res) {
 
 function updateInventory(items) { //updates inventory in firebase
   return new Promise((resolve, reject) => {
-    let inventory = fetch(`${server}/api/inventory/GetAllItems`); 
-    //let inventory = fetch(process.env.GET_ALL_ITEMS); //twe can get rid of this 
-    inventory.then((value) => {
+    fetch(`${server}/api/inventory/GetAllItems`)
+    .then((value) => {
       value.json().then((inventoryJson) => {
          const inventoryUpdates = {}
          for (let item in items) {
@@ -61,53 +69,59 @@ function updateInventory(items) { //updates inventory in firebase
 }
 
 
-function addOrder(firstName, lastName, address, frequency, dependents, dietaryRestrictions, additionalRequests, calID, items, deliveryDate, deliveryWindow) {
-    //add order data to google sheets and firebase
-    // I used this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-    //add order data to three google sheets (pantry, bag-packing, doordash ) and add order to firebase
+function addOrder(body) {
+  let { firstName, lastName, address, frequency, dependents,
+        dietaryRestrictions, additionalRequests, calID, items,
+        deliveryDate, deliveryWindow, email, phone } =  body;
+
+  // I used this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+  
+  // add order data to three google sheets (pantry, bag-packing, doordash) and add order to firebase
   return new Promise((resolve, reject) => {
+
     const target = ['https://www.googleapis.com/auth/spreadsheets'];
-    var jwt = new google.auth.JWT(
-      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      //process.env.GOOGLE_SHEETS_CLIENT_EMAIL_TEST,
+    var sheets_auth = new google.auth.JWT(
+      client_email,
       null,
-      (process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-      //(process.env.GOOGLE_SHEETS_PRIVATE_KEY_TEST || '').replace(/\\n/g, '\n'),
+      (private_key || '').replace(/\\n/g, '\n'),
       target
     );
-    const sheets = google.sheets({ version: 'v4', auth: jwt });
 
-    //generate orderID: random six digit value. We check later to make sure that the item doesn't exist
+    const sheets = google.sheets({ version: 'v4', auth: sheets_auth });
+
+    /* Sheet 1: Food pantry master data sheet for tracking calIDs */
+    
+    // Generate orderID: random six digit value.
+    // We check later to make sure that the ID isn't in use already.
     const orderID = Math.random().toString().slice(2, 8);
     console.log("orderID", orderID);
 
     let d = new Date();
     const currentDate = (d.getMonth() + 1) + "/" + d.getDate();
-    //Food pantry master data sheet for tracking calIDs 
-    //current schema is [current date, CalID (encrypted), unique order id]
+    
+    // Schema is [current date, CalID (encrypted), unique order id, email]
     const request1 = {
-      //spreadsheetId: process.env.SPREADSHEET_ID_TEST,//PANTRY_DATA_SPREADSHEET_ID, 
-      spreadsheetId: process.env.PANTRY_DATA_SPREADSHEET_ID,
-      //range: "Sheet1!A:C", 
-      range: "'TechTesting'!A:C",
+      spreadsheetId: pantry_sheet,
+      range: "'TechTesting'!A:D",
       valueInputOption: "USER_ENTERED", 
       insertDataOption: "INSERT_ROWS",
       resource: {
-          //"range": "Sheet1!A:C",
-          "range": "'TechTesting'!A:C",
+          "range": "'TechTesting'!A:D",
           "majorDimension": "ROWS",
           "values": [
-          [currentDate, calID, orderID] //each inner array is a row if we specify ROWS as majorDim
+            [currentDate, calID, orderID, email] //each inner array is a row if we specify ROWS as majorDim
           ] 
         } ,
-      auth: jwt
+      auth: sheets_auth
     }
 
-    
     sheets.spreadsheets.values.append(request1)
+    .then(()=> {
+      console.log("wrote successfully to pantry data sheet")
+    })
     .catch((error) => {
       return reject("error writing to Pantry data sheet: ", error);
-    });  
+    });
 
     //determine approximate # of bags by # of items
     var totalItems = 0;
@@ -120,41 +134,43 @@ function addOrder(firstName, lastName, address, frequency, dependents, dietaryRe
     }
 
 
-    /*bag packing sheet with [confirmed date (delivery date), first name + last initial, delivery window, # of bags, frequency, # of dependents, dietary restrictions,
-   additional requests, order id, items] 
-    frequency: one-time or recurring (once a week or every two weeks for now) */
+    /* Sheet 2: Bag packing sheet */
+    /* [confirmed date (delivery date), first name + last initial, delivery window, # of bags,
+     *  frequency, # of dependents, dietary restrictions, additional requests, order id, items] */
+
+    // frequency: one-time or recurring (once a week or every two weeks for now)
+
     const request2 = {
-      //spreadsheetId: process.env.SPREADSHEET_ID_TEST,//BAG_PACKING_SPREADSHEET_ID,
-      spreadsheetId: process.env.BAG_PACKING_SPREADSHEET_ID,
-      //range: "Sheet1!A:J",
+      spreadsheetId: bag_packing_sheet,
       range: "[Testing] Spring Delivery Packing Info!A:J",
       valueInputOption: "USER_ENTERED", 
       insertDataOption: "INSERT_ROWS",
       resource: {
-          //range: "Sheet1!A:J",
           "range": "[Testing] Spring Delivery Packing Info!A:J",
           "majorDimension": "ROWS",
           "values": [
             [deliveryDate, firstName + " " + lastName.slice(0,1), deliveryWindow, numberOfBags, frequency, 
-          dependents, dietaryRestrictions, additionalRequests, orderID, JSON.stringify(items)]
+             dependents, dietaryRestrictions, additionalRequests, orderID, JSON.stringify(items)]
           ] 
-        } 
+        }
     }
+
     sheets.spreadsheets.values.append(request2)
+    .then(()=> {
+      console.log("wrote successfully to bag packing sheet")
+    })
     .catch((error) => {
       return reject("error writing to bag-packing data sheet: ", error);
     });
-
-    //door dash sheet with [deliveryDate, delivery window start/end, first name, last NAME, address, item code (always F), # of bags (must be <= 3) ]
+  
+    /* Sheet 3: DoorDash information */
+    /* [deliveryDate, delivery window start/end, first name, last NAME, address, item code (always F), # of bags (must be <= 3) ] */
     const request3 = {
-      //spreadsheetId: process.env.SPREADSHEET_ID_TEST,//DOORDASH_SPREADSHEET_ID,
-      spreadSheetId: process.env.DOORDASH_SPREADSHEET_ID,
-      //range: "Sheet1!A:H",
+      spreadSheetId: doordash_sheet,
       range: "Customer Information!A:H",
-      valueInputOption: "USER_ENTERED", 
+      valueInputOption: "USER_ENTERED",
       insertDataOption: "INSERT_ROWS",
       resource: {
-          //range: "Sheet1!A:H",
           "range": "Customer Information!A:H",
           "majorDimension": "ROWS",
           "values": [
@@ -162,15 +178,17 @@ function addOrder(firstName, lastName, address, frequency, dependents, dietaryRe
           ] 
         } 
     }
+    
     sheets.spreadsheets.values.append(request3)
+    .then(()=> {
+      console.log("wrote successfully to doordash sheet")
+    })
     .catch((error) => {
       return reject("error writing to Doordash data sheet: ", error);
     });
 
 
-    /*add order to firebase. I used AddItem code
-    */
-
+    /* Add order to firebase */
     let newOrder = {};
     newOrder["orderID"] = orderID;
     newOrder["status"] = "Not started";
@@ -183,35 +201,36 @@ function addOrder(firstName, lastName, address, frequency, dependents, dietaryRe
     newOrder["lastInitial"] = lastName.slice(0, 1);
 
     firebase.auth().signInAnonymously()
-            .then(() => {
-              console.log("orderID", orderID);
-              let itemRef = firebase.database().ref('/order/' + orderID);
-              itemRef.once('value')
-              .catch(function(error){
-                console.log("Not ok5")
-                res.status(500).json({error: "server error getting reference to that item from the database", errorstack: error});
-                return resolve();
-            })
-            .then(function(resp){
-              // the version of the item in the database
-              var dbItem = resp.val();
-              // this item already exists
-              if (dbItem != null) {
-                  console.log("Not ok6")
-                  return resolve();
-              }
-              // otherwise the item doesn't exist and we can create it
-              itemRef.update(newOrder)
-              .catch(function(error) {
-                  console.log("Not ok7: ", error)
-                  return resolve();
-              })
-              .then(() => {
-                  console.log("OK")
-                  return resolve();
-              });
-          });
-            });
+    .then(() => {
+      console.log("orderID", orderID);
+      let itemRef = firebase.database().ref('/order/' + orderID);
+
+      itemRef.once('value')
+      .catch(function(error){
+        console.log("Not ok5")
+        res.status(500).json({error: "server error getting reference to  from the database", errorstack: error});
+        return resolve();
+      })
+      .then(function(resp){
+        // the version of the item in the database
+        var dbItem = resp.val();
+        // this item already exists
+        if (dbItem != null) {
+            console.log("Not ok6")
+            return resolve();
+        }
+        // otherwise the item doesn't exist and we can create it
+        itemRef.update(newOrder)
+        .catch(function(error) {
+            console.log("Not ok7: ", error)
+            return resolve();
+        })
+        .then(() => {
+            console.log("OK")
+            return resolve();
+        });
+    });
+    });
 
     return resolve();
 
@@ -222,26 +241,29 @@ export default async function(req, res) {
     // verify this request is legit
     const token = req.headers.authorization
     console.log("TOKEN: ", token)
+
     const allowed = await validateFunc(token)
     if (!allowed) {
         res.status(401)
         res.json({error: "you are not authenticated to perform this action"})
-        return Promise.reject();
+        return Promise.resolve();
     } 
+
     const {body} = req //unpacks the request object 
     if (!body.frequency) {
       body.frequency = "one-time";
     }
     let ok = requireParams(body, res); 
     if (!ok) {
-      return Promise.reject();
+      res.status(401)
+      res.json({error: "you are not authenticated to perform this action"})
+      return Promise.resolve();
     } 
+
     firebase.auth().signInAnonymously()
     .then(() => {
       updateInventory(body.items).then((success) => {
-        addOrder(body.firstName, body.lastName, body.address, body.frequency, body.dependents, 
-          body.dietaryRestrictions, body.additionalRequests,
-          body.calID, body.items, body.deliveryDate, body.deliveryWindow).then(() => {
+        addOrder(body).then(() => {
             console.log("Added to google sheets");
           })
           .catch((error) => {
