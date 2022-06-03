4d9fb500e2e31fa1f5f8217d920e07391d9debbc
Janise Liang <jliang5067@gmail.com>
2022-06-03 19:04:01 -0400

    add editable homepage markdown

---
+++ pages/api/admin/GetHomeInfo.js
@@ -0,0 +1,25 @@
+import firebase from '../../../firebase/clientApp'
+
+/*
+* /api/orders/GetHomeInfo
+*/
+
+export default async function(req,res) {
+
+  return new Promise((resolve, reject) => {
+
+    // no need to sign in since we're just reading
+    firebase.database()
+    .ref('/info/homepage/')
+    .once('value')  
+    .catch(function(error){
+      res.status(500).json({error: "server error getting info", errorstack: error});
+      return resolve();
+    })
+    .then(function(resp){
+      var markdown = resp.val();
+      res.status(200).json({markdown: markdown});
+      return resolve();
+    });
+  })
+}
