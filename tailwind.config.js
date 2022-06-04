5235e87fecb628ec59e32c7c69c4e00930b78c04
Janise Liang <jliang5067@gmail.com>
2022-06-04 00:14:40 -0400

    updating for tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     // Extend rather than override theme defaults for now
     extend: {
