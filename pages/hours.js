32e9fb2fc8355b07870e68ce2b67da5a51a60631
Janise Liang <jliang5067@gmail.com>
2022-06-06 16:07:55 -0400

    simplifying and cleaning up some HTML tags

--- pages/hours.js
+++ pages/hours.js
@@ -12,145 +12,118 @@ const token = cookie.get("firebaseToken")
 
 export default function Hours() {
 
-    var dayToHours;
-    var dayObjects = [];
+  // var dayToHours;
+  // var dayObjects = [];
 
-    /**
-     * Fetch the current hours of the pantry and convert them to a map with
-     * the format being day:hours
-     */
-    const fetchHours = async () => {
+  // /**
+  //  * Fetch the current hours of the pantry and convert them to a map with
+  //  * the format being day:hours
+  //  */
+  // const fetchHours = async () => {
 
-        await fetch('/api/admin/getHours', {
-            method: 'GET',
-            headers: { 'Content-Type': "application/json" }
-        })
-            .then(function (res) {
-                res.json().then(json => {
-                    dayToHours = json.message
-                    dayToHours = new Map(dayToHours)
-                    createDayObjects()
-                });
+  //   await fetch('/api/admin/getHours', {
+  //     method: 'GET',
+  //     headers: { 'Content-Type': "application/json" }
+  //   })
+  //   .then(function (res) {
+  //     res.json().then(json => {
+  //       dayToHours = json.message
+  //       dayToHours = new Map(dayToHours)
+  //       createDayObjects()
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log("error:", error)
+  //   });
+  // }
 
-            })
-            .catch(function (error) {
-            });
+  // /**
+  //  * Convert the map into objects with day and hour properties
+  //  */
+  // const createDayObjects = () => {
+  //   const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 
+  //   for (let day in days) {
+  //     const dayObject = new Object();
+  //     dayObject.day = days[day];
+  //     dayObject.hours = dayToHours.get(days[day])
+  //     dayObjects.push(dayObject);
+  //   }
+  // }
 
+  // useEffect(() => {
+  //   fetchHours();
+  // }, []);
 
-    }
+  const { data, error } = useSWR('/api/admin/getHours', fetcher)
 
-    /**
-     * Convert the map into objects with day and hour properties
-     * 
-     */
-    const createDayObjects = () => {
-        const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  /* Display error message */
+  if (error) {
+    return (
+      <>
+        <Head>
+          <title>Pantry</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
+        </Head>
+        <Layout>
+          <div className='p-5'>Failed to get hours</div>
+        </Layout>
+      </>
+    )
+  }
 
-        for (let day in days) {
-            const dayObject = new Object();
-            dayObject.day = days[day];
-            dayObject.hours = dayToHours.get(days[day])
-            dayObjects.push(dayObject);
-        }
+  /* Display loading message */
+  if (!data) {
+    return (
+      <>
+        <Head>
+          <title>Pantry</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
+        </Head>
+        <Layout>
+          <div className='p-5'>Fetching hours...</div>
+        </Layout>
+      </>
+    )
+  }
 
-    }
+  /* otherwise display table of hours */
+  const daysInOrder = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  return (
+    <>
+      <Head>
+        <title>Pantry</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Layout>
+        <h1 className="text-3xl flex justify-center items-center">Pantry Hours</h1>
+        <table className="text-xl flex justify-center items-center" cellpadding="10" cellspacing="10">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              daysInOrder.map((dayKey) => (
+                <tr key={dayKey}>
+                  <td>{dayKey[0].toUpperCase() + dayKey.substring(1)}</td>
+                  <td>
+                    {data[dayKey]}
+                  </td>
 
-    useEffect(() => {
+                </tr>
+              ))
+            }
 
-        fetchHours();
-    }, []);
-
-
-    
-
-
-    const { data, error } = useSWR('/api/admin/getHours', fetcher)
-    if (error) {
-        return (
-            <>
-                <Head>
-                    <title>Pantry</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
-                </Head>
-                <Layout>
-                    <th>Failed to get hours</th>
-                </Layout>
-            </>
-        )
-    }
-    if (!data) {
-        return (
-            <>
-                <Head>
-                    <title>Pantry</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
-                </Head>
-                <Layout>
-                    <th>Fetching hours...</th>
-                </Layout>
-            </>
-        )
-    }
-    else {
-        dayToHours = data.message
-        dayToHours = new Map(dayToHours)
-        createDayObjects()
-        return (
-            <>
-                <Head>
-                    <title>Pantry</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
-                </Head>
-                <Layout>
-                    <div className="container">
-                        <h1 class="text-3xl flex justify-center items-center">Pantry Hours</h1>
-                        <table class="text-xl flex justify-center items-center" cellpadding="10" cellspacing="10">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    dayObjects.map((item) => (
-                                        <tr key={item.day}>
-                                            <td>{item.day[0].toUpperCase() + item.day.substring(1)}</td>
-                                            <td>
-                                                {item.hours}
-                                            </td>
-                                            
-                                        </tr>
-                                    ))
-                                }
-
-                            </tbody>
-                        </table>
-                    </div>
-                </Layout>
-            </>
-
-
-
-
-        )
-    }
+          </tbody>
+        </table>
+      </Layout>
+    </>
+  )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
