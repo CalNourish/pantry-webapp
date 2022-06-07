0a38b22a85f0a5305af3b7d7b27bc3cd76e1ec78
Janise Liang <jliang5067@gmail.com>
2022-06-07 01:58:20 -0400

    consolidated admin hours with hours page

--- pages/hours.js
+++ pages/hours.js
@@ -1,80 +1,54 @@
 import Layout from '../components/Layout'
 import Head from 'next/head'
-import useSWR from 'swr'
-
 import { useEffect, useState } from 'react'
-import { useUser } from '../context/userContext'
 import cookie from 'js-cookie';
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+
+import { useUser } from '../context/userContext'
+import { server } from './_app.js'
+
+// const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const token = cookie.get("firebaseToken")
+const daysInOrder = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 
 export default function Hours() {
 
-  // var dayToHours;
-  // var dayObjects = [];
+  const [editing, setEditing] = useState(false);
+  const [data, setData] = useState(undefined);
+  const { user } = useUser();
 
-  // /**
-  //  * Fetch the current hours of the pantry and convert them to a map with
-  //  * the format being day:hours
-  //  */
-  // const fetchHours = async () => {
-
-  //   await fetch('/api/admin/getHours', {
-  //     method: 'GET',
-  //     headers: { 'Content-Type': "application/json" }
-  //   })
-  //   .then(function (res) {
-  //     res.json().then(json => {
-  //       dayToHours = json.message
-  //       dayToHours = new Map(dayToHours)
-  //       createDayObjects()
-  //     });
-  //   })
-  //   .catch(function (error) {
-  //     console.log("error:", error)
-  //   });
-  // }
-
-  // /**
-  //  * Convert the map into objects with day and hour properties
-  //  */
-  // const createDayObjects = () => {
-  //   const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-
-  //   for (let day in days) {
-  //     const dayObject = new Object();
-  //     dayObject.day = days[day];
-  //     dayObject.hours = dayToHours.get(days[day])
-  //     dayObjects.push(dayObject);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchHours();
-  // }, []);
-
-  const { data, error } = useSWR('/api/admin/getHours', fetcher)
-
-  /* Display error message */
-  if (error) {
-    return (
-      <>
-        <Head>
-          <title>Pantry</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
-        </Head>
-        <Layout>
-          <div className='p-5'>Failed to get hours</div>
-        </Layout>
-      </>
-    )
+  // let { data, error } = useSWR('/api/admin/getHours', fetcher)
+  let getHours = () => {
+    fetch(`${server}/api/admin/getHours`)
+    .then((result) => {
+      result.json()
+      .then((data) => {
+        setData(data);
+      });
+    })
+    .catch(err => {
+      /* Display error message */
+      console.log("getHours error:", err)
+      return (
+        <>
+          <Head>
+            <title>Pantry</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
+          </Head>
+          <Layout>
+            <div className='p-5'>Failed to get hours</div>
+          </Layout>
+        </>
+      )
+    });
   }
 
   /* Display loading message */
   if (!data) {
+    getHours();
+
     return (
       <>
         <Head>
@@ -89,8 +63,34 @@ export default function Hours() {
     )
   }
 
+  let authToken = (user && user.authorized === "true") ? token : null;
+
+  let saveHours = (day, event) => {
+    event.preventDefault();
+    let hourString = event.target[`hours-${day}`].value
+
+    fetch('/api/admin/updateHours', {
+      method: 'POST',
+      body: JSON.stringify({
+          "day": day,
+          "hours": hourString,
+      }),
+      headers: { 'Content-Type': "application/json", 'Authorization': token }
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+      throw response.status
+    })
+    .then(json => {
+      setEditing(false)
+      setData({...data, [day]: hourString}); // update the data object
+    })
+    .catch(errcode => {
+      console.log("Error updating hours: error code", errcode)
+    })
+  }
+
   /* otherwise display table of hours */
-  const daysInOrder = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
   return (
     <>
       <Head>
@@ -99,30 +99,41 @@ export default function Hours() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Rubik:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <Layout>
-        <h1 className="text-3xl flex justify-center items-center">Pantry Hours</h1>
-        <table className="text-xl flex justify-center items-center" cellpadding="10" cellspacing="10">
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              daysInOrder.map((dayKey) => (
-                <tr key={dayKey}>
-                  <td>{dayKey[0].toUpperCase() + dayKey.substring(1)}</td>
-                  <td>
-                    {data[dayKey]}
-                  </td>
+        <div className='flex flex-col items-center m-4'>
+          <h1 className="text-3xl flex text-center tracking-wide font-semibold">Pantry Hours</h1>
+          <table className="text-xl" cellPadding="10" cellSpacing="10">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                daysInOrder.map((dayKey) => (
+                  <tr key={dayKey}>
+                    <td>{dayKey[0].toUpperCase() + dayKey.substring(1)}</td>
+                    <td>
+                      {(authToken && (editing==dayKey)) ? 
+                        <form onSubmit={(event) => saveHours(dayKey, event)}>
+                          <input className='border rounded border-gray-600 bg-gray-300 pl-2 w-auto mr-4' defaultValue={data[dayKey]} id={`hours-${dayKey}`}/>
+                          <button type="submit" className='text-sm text-blue-600 hover:text-blue-400 focus:outline-none'>save</button>
+                          <button onClick={() => setEditing(false)} className='ml-4 text-sm text-blue-600 hover:text-blue-400 focus:outline-none'>cancel</button>
+                        </form>
+                        :
+                        <>
+                          <span className='mr-4 border border-transparent' onDoubleClick={() => setEditing(dayKey)}>{data[dayKey]}</span>
+                          {authToken && <button className='text-sm text-blue-600 hover:text-blue-400 focus:outline-none' onClick={() => setEditing(dayKey)}>edit</button>}
+                        </>
+                      }
+                    </td>
+                  </tr>
+                ))
+              }
 
-                </tr>
-              ))
-            }
-
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </Layout>
     </>
   )
