65a1fca20c710f5b698c3c4a01f5682165dbc7eb
Justin Zhang <zhangjustin77@gmail.com>
2022-07-23 18:33:20 -0700

    Added some logic for scanning table and formatting date

--- pages/api/admin/CheckIn.js
+++ pages/api/admin/CheckIn.js
@@ -19,6 +19,41 @@ function requireParams(body, res) {
   return false;
 }
 
+
+//converts from 2022-07-23T20:35:41.935Z to 7/23/2022 12:15:52
+function formatTime(timeToConvert) {
+    const formattedHours = timeToConvert.getHours()+":"+timeToConvert.getMinutes()+":"+timeToConvert.getSeconds()
+    const formattedTime = timeToConvert.toLocaleDateString() + " " + formattedHours
+    return formattedTime
+}
+
+function getNumRowsForCheckIn(properties) {
+    for (var sheet of properties["data"]["sheets"]) {
+        if(sheet.properties.title == "Check Out Form") {
+          return sheet.properties.gridProperties.rowCount
+        }
+    }
+}
+
+function checkForVisitInLastWeek(numRows,calID,sheets) {
+  let numberOfRowsToGoBack = 10
+  var rangeQuery = "Check Out Form!A:B"
+  var startingRow = numRows - numberOfRowsToGoBack 
+  if (startingRow > 0) { //number of rows we want to scan is larger than table so scan whole table
+    rangeQuery = "Check Out Form!A"+(startingRow).toString()+":B"
+  }
+  const paramsForVisits = {
+    spreadsheetId: checkin_sheet,
+    range: rangeQuery,
+  }
+  const ID = sheets.spreadsheets.values.get(paramsForVisits)
+  .then((result) => console.log(result.data))
+  .catch((error) => {
+    return console.log("error reading from Pantry data sheet: ", error);
+  });
+}
+
+
 export default async function(req, res) {
   console.log("Checking In")
   const token = req.headers.authorization
@@ -45,23 +80,38 @@ export default async function(req, res) {
         target,
         null,
         service_info.private_key_id
-      );
+      )
 
+      // for (sheet in spreadsheet.sheets) {
+      //     Timber.d("sheetId=${sheet.properties.sheetId}, title=${sheet.properties.title}, rows=${sheet.properties.gridProperties.rowCount}")
+      // }
+      //read from google sheets to determine last visit
+      const paramsForCheckIn = {
+        spreadsheetId: checkin_sheet,
+
+      }
       const sheets = google.sheets({ version: 'v4', auth: sheets_auth });
+      var numRows = 0;
+      sheets.spreadsheets.get(paramsForCheckIn).then((properties) => numRows = getNumRowsForCheckIn(properties))
+                                               .then(() => checkForVisitInLastWeek(numRows,body.calID,sheets))
+
+      
+
 
       // write to google sheets
       var timestamp = new Date();
+      timestamp.getDate
       const request = {
         spreadsheetId: checkin_sheet,
         range: "Check In!A:B",
         valueInputOption: "USER_ENTERED",
         insertDataOption: "INSERT_ROWS",
         resource: {
-            "range": "Check In!A:B",
+            "range": "Check Out From!A:B",
             "majorDimension": "ROWS",
             "values": [
               [
-                timestamp, body.calID
+                formatTime(timestamp), body.calID
               ]
             ]
           }
