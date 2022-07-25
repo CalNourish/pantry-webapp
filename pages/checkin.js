b1bcb8965249dd6a500f38de435db73fdd1bff79
Justin Zhang <zhangjustin77@gmail.com>
2022-07-24 17:46:37 -0700

    Disabled button for a little and improvements to focusing

--- pages/checkin.js
+++ pages/checkin.js
@@ -97,7 +97,18 @@ class Checkin extends React.Component {
   };
    
   handleScanSubmit = (e) => {
+    var fieldset = document.getElementById("calIDFieldset")
+    fieldset.disabled = true
+    setTimeout(()=>{
+      fieldset.disabled = false;
+      document.getElementById("calID").focus();
+      }, 1500)
+    
     e.preventDefault();
+    if (e.target.calID.value.length == 0 || e.target.calID.value == null) {
+      this.showError("Can't submit blank ID: " + e.target.calID.value,1000)
+      return
+    }
     fetch('/api/admin/CheckIn', { method: 'POST',
       body: JSON.stringify({calID: e.target.calID.value}),
       headers: {'Content-Type': "application/json", 'Authorization': token}
@@ -106,20 +117,32 @@ class Checkin extends React.Component {
       result.json()
       .then((lastVisitedTimes) => {
         this.setState({lastScannedID:e.target.calID.value, visitsLastWeek:lastVisitedTimes, lastScannedTime:new Date().toLocaleTimeString()})
-        this.showSuccess("Sucessfully scanned ID:" + e.target.calID.value,1000)
+        this.showSuccess("Sucessfully scanned ID: " + e.target.calID.value,1000)
         e.target.calID.value = null;
         document.getElementById("calID").focus();
       });
     })
     .catch((err) => {
-      this.showError("Failed scanning ID:" + e.target.calID.value,1000)
+      this.showError("Failed scanning ID: " + e.target.calID.value,1000)
     })
+  
   }
+
+  
 
   render() {
   /* feedback banners */
   const errorBanner = <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-3">{this.state.error}</div>;
   const successBanner = <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-3">{this.state.success}</div>;
+
+
+  document.onkeydown = (e) => {
+    if (!document.activeElement.classList.contains("calID") && !isNaN(e.key)) {
+      e.preventDefault();
+      document.getElementById("calID").focus();
+      document.getElementById("calID").value = e.key;
+    }
+  }
 
   return (
     <>
@@ -134,15 +157,18 @@ class Checkin extends React.Component {
           <h1 className='text-3xl font-medium mb-2'>Pantry Check-In</h1>
           <div className='flex flex-row space-x-8 my-8'>
             <form onSubmit={(e) => this.handleScanSubmit(e)}>
+              <fieldset id="calIDFieldset" disabled={false}>
               <div>
               <div className='flex-grow'>Use scanner or manually enter Cal ID (put 1 if general community member)</div>
-              <input className="border rounded w-2/3 py-2 px-3 text-gray-600 leading-tight"
+              <input className="calID border rounded w-2/3 py-2 px-3 text-gray-600 leading-tight"
                 placeholder="Cal ID"
                 id="calID" autoComplete="off" autoFocus></input>
               </div>
-              <input type="submit" className="btn my-1 btn-pantry-blue uppercase tracking-wide text-xs font-semibold flex-grow" value="Submit (Enter)" />
+              <input type="submit" id = "submitButton" className="btn my-1 btn-pantry-blue uppercase tracking-wide text-xs font-semibold flex-grow disabled:bg-pantry-blue-400" value="Submit (Enter)" />
+              </fieldset>
             </form>
             <div className = "w-1/3">
+
             {this.showLastScannedInfo()}
             {this.showLastVisitInfo()}
           </div>
