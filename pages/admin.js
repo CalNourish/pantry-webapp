f24d7087783c5baa6bb6a38083be71daab81c1a8
Janise Liang <jliang5067@gmail.com>
2022-09-14 21:09:32 -0700

    finished front end

--- pages/admin.js
+++ pages/admin.js
@@ -7,17 +7,14 @@ import { useEffect, useState } from 'react'
 import { useUser } from '../context/userContext'
 import { server } from './_app.js'
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+import { daysInOrder } from './hours';
 
 const token = cookie.get("firebaseToken")
 
-export default function Admin() {
+function SheetLinks(props) {
   const [submitStatus, setSubmitStatus] = useState({});
   const [statusTimer, setStatusTimer] = useState(null);
   const [formData, setFormData] = useState({});
-
-  const { user } = useUser();
-  let authToken = (user && user.authorized === "true") ? token : null;
 
   const fetcher = (url) => fetch(url, {
     method:'GET', headers: {'Content-Type': "application/json", 'Authorization': token}
@@ -25,26 +22,12 @@ export default function Admin() {
 
   const { data, error } = useSWR(`${server}/api/admin/GetSheetLinks`, fetcher);
 
-  if (!data) return <div>Loading...</div>
-  if (error || data.error) {
+  if (error) {
     return (
-      <Layout>
-        <div className='m-4'>Error! See console log for details.</div>
-      </Layout>
+      <div className='m-4'>Error! See console log for details.</div>
     )
   }
-
-  if (!authToken) {
-    return <>
-      <Head>
-        <title>Pantry</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-          <h1 className='text-xl m-6'>Sorry, you are not authorized to view this page.</h1>
-      </Layout>
-    </>
-  }
+  if (!data) return <div>Loading...</div>
 
   // initialize form info if empty
   if (Object.keys(formData).length == 0) {
@@ -145,16 +128,133 @@ export default function Admin() {
           {submitStatus[tag+"-msg"] || "X"}</div>}
       </form>
     )
-  } 
+  }
+
+  return (
+    <div className='m-8'>
+      <div className='font-semibold text-3xl mb-4'>Google Sheets Links</div>
+      <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+        {Object.keys(formData).map((tag) => generateForm(tag))}
+      </div>
+    </div>
+  )
+}
+
+function DeliveryTimes(props) {
+  const [formData, setFormData] = useState({});
+
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  let { data, error } = useSWR('/api/orders/GetDeliveryTimes', fetcher)
+
+  if (error) {
+    return (
+      <div className='m-4'>Error! See console log for details.</div>
+    )
+  }
+  if (!data) return <div>Loading...</div>
+
+  if (Object.keys(formData).length == 0) { // TODO: might need to change this
+    setFormData(data)
+  }
+
+  let deleteTime = (tag) => {
+    // TODO: call delete API
+    console.log('deleting tag:', tag)
+    let {[tag]: _, ...newData} = formData;
+    setFormData(newData)
+  }
+
+  let submitForm = (e) => {
+    e.preventDefault();
+
+    const date = e.target.date.value;
+    const start = e.target.start_hour.value;
+    const start_AM_PM = e.target.start_AM_PM.value;
+    const end = e.target.end_hour.value;
+    const end_AM_PM = e.target.end_AM_PM.value;
+
+    const tag = date.toLowerCase().substring(0,3) + start + "-" + end + end_AM_PM;
+    const displayName = `${date} ${start}-${end} ${end_AM_PM}`
+    const data = {
+      dayOfWeek: date,
+      display: displayName,
+      startTime: start + " " + start_AM_PM,
+      endTime: end + " " + end_AM_PM
+    }
+
+    // TODO: call add API
+    setFormData({...formData, [tag]: data})
+  }
+
+  return (
+    <div className='m-8'>
+      <div className='font-semibold text-3xl mb-4'>Delivery Time Windows</div>
+      {/* Existing times */}
+      <table className='mb-4'>
+        <tbody>
+          {Object.keys(formData).map((key) => <tr key={key}>
+            <td className='pr-10'>{formData[key].display}</td>
+            <td><img className="w-4 h-4 items-center hover:cursor-pointer" src="/images/trash-can.svg" onClick={() => deleteTime(key)}></img></td>
+          </tr>)}
+        </tbody>
+      </table>
+
+      {/* New times */}
+      <div className='border border-gray-400 bg-gray-50 p-4 w-fit'>
+        <div className='font-semibold text-xl mb-2'>Add a new time window:</div>
+        <form onSubmit={(e) => submitForm(e)}>
+          <div className='mb-2'>
+            <span className='font-semibold mr-2'>Date:</span>
+            <select id="date">
+              {daysInOrder.map((day) => <option key={day}>{day.charAt(0).toUpperCase() + day.slice(1)}</option>)}
+            </select>
+          </div>
+
+          <div className='mb-2'>
+            <span className='font-semibold mr-2'>Start time:</span>
+            <input id="start_hour" className='w-10 mr-2 border-b appearance-none focus:outline-none'></input>
+            <select id="start_AM_PM">
+              <option>AM</option>
+              <option>PM</option>
+            </select>
+          </div>
+          
+          <div className='mb-2'>
+            <span className='font-semibold mr-2'>End time:</span>
+            <input id="end_hour" className='w-10 mr-2 border-b appearance-none focus:outline-none'></input>
+            <select id="end_AM_PM">
+              <option>AM</option>
+              <option>PM</option>
+            </select>
+          </div>
+
+          <button type='submit' className='btn btn-outline p-2'>Add!</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default function Admin() {
+  const { user } = useUser();
+  let authToken = (user && user.authorized === "true") ? token : null;
+
+  if (!authToken) {
+    return <>
+      <Head>
+        <title>Pantry</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+          <h1 className='text-xl m-6'>Sorry, you are not authorized to view this page.</h1>
+      </Layout>
+    </>
+  }
 
   return (
     <Layout>
-      <div className='m-8'>
-        <div className='font-semibold text-3xl mb-4'>Google Sheets Links</div>
-        <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
-          {Object.keys(formData).map((tag) => generateForm(tag))}
-        </div>
-      </div>
+      <SheetLinks/>
+      <DeliveryTimes/>
     </Layout>
   )
 }
