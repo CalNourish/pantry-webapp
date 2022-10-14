35257d60a49623ca9decd09bf945a076b5af023e
Janise Liang <janise@berkeley.edu>
2022-10-14 16:39:36 -0700

    hardcode width of edit col

--- components/Table.js
+++ components/Table.js
@@ -142,7 +142,7 @@ export default function Table(props) {
             </div>
             <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                 <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                    <table className="min-w-full leading-normal">
+                    <table className="min-w-full leading-normal table-auto">
                         <thead>
                             <tr>
                                 <th onClick={() => {setSortBy(sortBy == "itemName" ? "-itemName" : "itemName")}}
@@ -171,7 +171,7 @@ export default function Table(props) {
                                     </div>
                                 </th>
                                 { props.authToken ?
-                                    <th className={headerClass}>Edit</th> : null}
+                                    <th className={headerClass + " w-28"}>Edit</th> : null}
                             </tr>
                         </thead>
                         <tbody>
