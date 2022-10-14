195e221b016e9acfcada31360bb8d0e98939d318
Janise Liang <janise@berkeley.edu>
2022-10-14 16:58:30 -0700

    set max checkout quantity to 1000

--- pages/api/inventory/CheckoutItems.js
+++ pages/api/inventory/CheckoutItems.js
@@ -18,6 +18,8 @@ import { google } from 'googleapis';
 
 import { service_info } from "../../../utils/decrypt.js"
 
+const CHECKOUT_MAX = 1000;
+
 function requireParams(body, res) {
   /* require elements: array of elements {barcode: quantity} */
 
@@ -31,6 +33,10 @@ function requireParams(body, res) {
   for (let barcode in body) {
     if (!parseInt(body[barcode])) {
       res.status(400).json({ error: `Unable to parse quantity for barcode ${barcode}: '${body[barcode]}'` })
+      return false;
+    }
+    if (body[barcode] > CHECKOUT_MAX) {
+      res.status(400).json({ error: `Quantity of ${barcode} exceeds max quantity (${body[barcode]} > ${CHECKOUT_MAX}).`})
       return false;
     }
   }
