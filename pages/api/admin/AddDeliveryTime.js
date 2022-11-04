0884c58cb78b415201d0cdbbe9880fd4d12f37f4
Janise Liang <jliang5067@gmail.com>
2022-11-04 15:04:50 -0700

    finish 1st draft of add/delete delivery windows APIs

--- pages/api/admin/AddDeliveryTime.js
+++ pages/api/admin/AddDeliveryTime.js
@@ -63,13 +63,33 @@ export default async function (req, res) {
       // perform the write
       firebase.auth().signInAnonymously()
       .then(() => {
-        let database_key = day.substring(0,3).toLowerCase() + start + "-" + end
+        let db_tag = day.substring(0,3).toLowerCase() + start + "-" + end
         let payload = {
           "dayOfWeek": day,
           "display": `${day} ${start}-${end} ${end_AMPM}`,
           "startTime": start + " " + start_AMPM,
           "endTime": end + " " + end_AMPM
         }
+
+        let windowRef = firebase.database().ref("/deliveryTimes/" + db_tag);
+        windowRef.once('value')
+          .then((resp) => {
+            var window = resp.val();
+            if (window != null) {
+              res.status(400).json({ error: "delivery time with tag " + db_tag + " already exists" })
+              return resolve();
+            }
+
+            windowRef.update(payload)
+            .then(() => {
+              res.status(200).json({ message: "success" });
+              return resolve();
+            })
+            .catch(function (error) {
+              res.status(500).json({ error: "error adding delivery window", errorstack: error });
+              return resolve();
+            });
+          })
       })
       .catch(() => {
         res.status(500).json({error: "Error signing in to firebase: " + err});
@@ -82,80 +102,3 @@ export default async function (req, res) {
     });
   })
 }
-
-/** Rough start to a function used to validate hours of the food pantry, currently
- *  not utilized. TODO
- */
-export function validateHours(hours) {
-
-  let trimmedHours = hours.trim()
-  if (trimmedHours.toString() === "Closed" || trimmedHours.toString() === "closed") {
-    return true;
-  }
-
-  let brokenUpHours = trimmedHours.split(" ");
-  if (brokenUpHours.length !== 5) {
-    return false;
-  }
-
-  let startTime = brokenUpHours[0]
-  let startMerdiem = brokenUpHours[1].toString()
-  let endTime = brokenUpHours[3]
-  let endMerdiem = brokenUpHours[4].toString()
-  let brokenUpStartTime = startTime.split(':');
-  let brokenUpEndTime = endTime.split(':');
-  if (brokenUpEndTime.length !== 2 || brokenUpStartTime.length !== 2) {
-    console.log("Bad input2");
-    return false;
-  }
-
-  let [startHr, startMn] = startTime.split(':');
-  let [endHr, endMn] = endTime.split(':');
-  startHr = parseInt(startHr)
-  startMn = parseInt(startMn)
-  endHr = parseInt(endHr)
-  endMn = parseInt(endMn)
-  if (isNaN(startMn) || isNaN(startHr) || startHr > 12 || startHr < 1 || startMn > 59 || startMn < 0) {
-    console.log("Bad input3");
-    return false;
-  }
-  if (isNaN(endMn) || isNaN(endHr) || endHr > 12 || endHr < 1 || endMn > 59 || endMn < 0) {
-
-    console.log("Bad input4");
-    return false;
-  }
-  if ((startMerdiem !== "AM" && startMerdiem !== "PM") || (endMerdiem !== "AM" && endMerdiem !== "PM")) {
-    console.log("Bad input5");
-    console.log(startMerdiem !== "AM")
-    console.log(endMerdiem)
-    return false;
-
-  }
-
-  let militaryMinutesStart = convertTime12to24toMinutes(startHr, startMn, startMerdiem);
-  let militaryMinutesEnd = convertTime12to24toMinutes(endHr, endMn, endMerdiem);
-  if (militaryMinutesEnd <= militaryMinutesStart) {
-    console.log("Bad input6");
-  }
-  return true;
-
-}
-
-/** 
- * Helper function for validateHours to convert from standard time to military time
- */
-const convertTime12to24toMinutes = (hours, minutes, merdiem) => {
-
-  if (hours === 12) {
-    hours = 0;
-  }
-
-  if (merdiem === 'PM') {
-    hours = hours + 12;
-  }
-
-  return hours * 60 + minutes;
-
-}
-
-
