92101322db3825717e82dd92e644d260e12ec70c
Janise Liang <janise@berkeley.edu>
2022-12-02 16:34:43 -0800

    change all tab names

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,22 @@
+import Head from "next/head";
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function Layout(props) {
   return (
-    // <div>
-    <div className="h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow flex-auto flex-shrink-0">
-        {props.children}
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Food Pantry</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow flex-auto flex-shrink-0">
+          {props.children}
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
