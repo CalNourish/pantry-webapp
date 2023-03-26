e13e8a71d09765deadb25cf5bcb598dcf26fe7fe
Janise Liang <jliang5067@gmail.com>
2023-03-26 01:13:52 -0700

    change api/validate response to json

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -33,29 +33,25 @@ export const validateFunc = async (token) => {
 };
 
 export default async (req, res) => {
-  try {
-    // Check if there is a token and if not return undefined.
-    
-    const { token } = JSON.parse(req.headers.authorization || '{}');
-    if (!token) {
-      return res.status(403).send({
-        errorCode: 403,
-        message: 'Auth token missing.',
-      });
-    }
-    // Call the validate function above that gets the user data
-    // this data ends up getting passed to every page's props
-    const authenticated = await validateFunc(token);
+  // Check if there is a token and if not return undefined.
+  const { token } = JSON.parse(req.headers.authorization || '{}');
+  if (!token) {
+    return res.status(403).json({
+      message: 'Auth token missing.'
+    });
+  }
+  
+  // Call the validate function above that gets the user data
+  // this data ends up getting passed to every page's props
+  validateFunc(token).then(() => {
     const result = {
       "user": {
         "Data": "Hello"
       },
     };
-    return res.status(200).send(JSON.stringify(result));
-  } catch (err) {
-    // Return undefined if there is no user. You may also send a different status or handle the error in any way that you wish.
-    console.log(err);
-    const result = undefined;
-    return res.status(200).send(result);
-  }
+    return res.status(200).json(result);
+  }).catch(err => {
+    console.log(err)
+    return res.status(403).json({message: "No user found."});
+  })
 };
