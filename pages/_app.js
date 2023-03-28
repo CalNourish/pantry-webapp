329d835fd6ea4e1e3bb106b10cdda502f8770d96
Janise Liang <jliang5067@gmail.com>
2023-03-27 21:44:31 -0700

    comment out getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -19,26 +19,26 @@ export default function MyApp({ Component, pageProps }) {
 
 // this stuff puts stuff into props...do we need to do that if everything is stored in the context?
 // when you remove this there are some issues with making CORS requests...why?
-MyApp.getInitialProps = async (appContext) => {
-  const { ctx } = appContext;
-  // Calls `getInitialProps` and fills `appProps.pageProps`
-  let error;
-  const appProps = await App.getInitialProps(appContext);
+// MyApp.getInitialProps = async (appContext) => {
+//   const { ctx } = appContext;
+//   // Calls `getInitialProps` and fills `appProps.pageProps`
+//   let error;
+//   const appProps = await App.getInitialProps(appContext);
 
-  const { firebaseToken } = cookies(ctx);
+//   const { firebaseToken } = cookies(ctx);
 
-  // If token exists run Firebase validation on server side before rendering.
-  if (firebaseToken) {
-    try {
-      const headers = {
-        'Context-Type': 'application/json',
-        Authorization: JSON.stringify({ "token": firebaseToken }),
-      };
-      const result = await fetch(`${server}/api/validate`, { headers }).then((res) => res.json());
-      return { ...result, ...appProps };
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  return { ...appProps };
-};
+//   // If token exists run Firebase validation on server side before rendering.
+//   if (firebaseToken) {
+//     try {
+//       const headers = {
+//         'Context-Type': 'application/json',
+//         Authorization: JSON.stringify({ "token": firebaseToken }),
+//       };
+//       const result = await fetch(`${server}/api/validate`, { headers }).then((res) => res.json());
+//       return { ...result, ...appProps };
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+//   return { ...appProps };
+// };
