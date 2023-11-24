2097fd8f017287158092c5d3be5674e8bebbb948
Richard Yu <richardyu@berkeley.edu>
2023-11-24 12:26:04 -0800

    Create button to copy visualization to clipboard

--- components/BarGraph.js
+++ components/BarGraph.js
@@ -43,6 +43,31 @@ export default function BarGraph({ data }) {
         a.click()
     }
 
+    const copyVisualization = () => {
+        const chartInstance = chart.current
+        const base64Image = chartInstance.toBase64Image()
+
+        const base64ImageWithoutPrefix = base64Image.replace(/^data:[^;]+;base64,/, '');
+        const binaryData = window.atob(base64ImageWithoutPrefix);
+
+        const uint8Array = new Uint8Array(binaryData.length);
+        for (let i = 0; i < binaryData.length; i++) {
+            uint8Array[i] = binaryData.charCodeAt(i);
+        }
+
+        const base64ImageBlob = new Blob([uint8Array], { type: "image/png" });
+
+        try {
+            navigator.clipboard.write([
+                new ClipboardItem({
+                    "image/png": base64ImageBlob
+                })
+            ])
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     const options = {
         responsive: true,
         plugins: {
@@ -90,7 +115,10 @@ export default function BarGraph({ data }) {
     return (
         <div style={{ position: "relative", margin: "auto", width: "75vw" }}>
             <Bar options={options} data={data} ref={chart} />
-            <button className="my-2 btn btn-pantry-blue w-full uppercase tracking-wide text-xs font-semibold focus:shadow-none w-24" onClick={() => exportVisualization()}>
+            <button className="my-2 mx-1 btn btn-pantry-blue w-full uppercase tracking-wide text-xs font-semibold focus:shadow-none w-20" onClick={() => copyVisualization()}>
+                Copy
+            </button>
+            <button className="my-2 mx-1 btn btn-pantry-blue w-full uppercase tracking-wide text-xs font-semibold focus:shadow-none w-24" onClick={() => exportVisualization()}>
                 Export
             </button>
         </div>
