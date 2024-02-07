4c1a0924fea3f5034050ac075d49cd7ebf02a3c5
Richard Yu <richardyu@berkeley.edu>
2024-02-06 17:32:58 -0800

    Improve input styling consistency

--- pages/visualizations.js
+++ pages/visualizations.js
@@ -10,7 +10,7 @@ const fetcher = (url) => {
 
 // TODO: AUTHENTICATION
 
-const inputAppearance = "appearance-none block w-full text-gray-600 border border-gray-100 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
+const inputAppearance = "appearance-none h-full rounded-l rounded-r border block w-full bg-white border-gray-300 text-gray-600 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
 
 const historyTimeFrame = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -180,13 +180,22 @@ class Visualization extends React.Component {
                                 ? (<>(Selected: {this.state.selectedItem})</>)
                                 : null
                             }</label>
-                            <input placeholder="Search name" onChange={(e) => {this.setSearch(e.target.value)}}
-                                className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-300 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-300 text-gray-600 focus:bg-white focus:placeholder-gray-500 focus:text-gray-600 focus:outline-none" />
+                            <div className="block relative">
+                                <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                                    <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-400">
+                                        <path
+                                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <input placeholder="Search name" onChange={(e) => {this.setSearch(e.target.value)}}
+                                className="appearance-none rounded-r rounded-l border border-gray-300 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-300 text-gray-600 focus:bg-white focus:placeholder-gray-500 focus:text-gray-600 focus:outline-none" />
+                            </div>
                             {
                                 this.search 
                                 ? this.state.searchResults.map((item, index) => (
                                     <>
-                                        <p onClick={(e) => this.setSelectedItem(item)}>{item}</p>
+                                        <p className="cursor-pointer" onClick={(e) => this.setSelectedItem(item)}>{item}</p>
                                         <hr />
                                     </>
                                 )) 
