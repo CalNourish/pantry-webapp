2441a6635ae02ac81db8da5c18ecdd208fb41c9d
lunava <luisnavarreterios@gmail.com>
2024-02-10 00:45:52 -0800

    created api files for limiteditems

---
+++ pages/api/admin/GetLimitedItems.js
@@ -0,0 +1,13 @@
+import firebase from "../../../firebase/clientApp"
+
+/*
+* /api/admin/GetlimitedItems
+*  returns the limited items data for checkout page
+ */
+
+
+export default async function (_, res) {
+   return new Promise ((resolve) =>  {
+      
+   })
+}
---
+++ pages/api/admin/SetLimitedItems.js
@@ -0,0 +1,8 @@
+import { getAuth, signInAnonymously } from 'firebase/auth';
+import firebase from '../../../firebase/clientApp'
+import {validateFunc} from '../validate'
+
+/*
+* req.body = TODO: structure 
+*/
+
