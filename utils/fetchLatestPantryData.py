7acf094d0e92cc17c2c6b91386ae9b781f074f34
Richard Yu <richardyu@berkeley.edu>
2024-05-21 21:42:44 -0700

    Remove old visualizations implementation

--- utils/fetchLatestPantryData.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def fetch_pantry_data(spreadsheet_id):
-    url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=csv"
-    df = pd.read_csv(url)
-    df.to_csv("utils/data.csv")
-
-fetch_pantry_data("1waiCSO7hm8kmWLaG8OdB8_7ksfEbm_B3CyKHbXTIf5E")
