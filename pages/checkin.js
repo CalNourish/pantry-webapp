fc1bf355bf8d3933233cdf7af09f03c75dfc43a7
pphilipngo <pphilipngo@gmail.com>
2025-07-23 16:16:02 -0700

    Can scan or type in id (#234)
    Can either accept 9 digit IDs or 10 digit IDs (barcode scan or manual input)

--- pages/checkin.js
+++ pages/checkin.js
@@ -135,7 +135,7 @@ class Checkin extends React.Component {
   validateCalId = (calIdValue) => {
     const regexes = { 
       // old: 'isStudent': /^(30\d{8}|[1278]\d{7})$/,
-      'isStudent': /^\d{8}$/,
+      'isStudent': /^(\d{8}|\d{10})$/,
       'isEmployeeOrAffiliate': /^(01\d{6}|10\d{6})$/,
       'isValidEncrypted': /^810:\d{8}$/,
       'isCommunity': /^1$/
