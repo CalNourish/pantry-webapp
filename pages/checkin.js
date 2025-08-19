9e44eacf7fea9ae7035d7ec48788a66c5d271811
pphilipngo <pphilipngo@gmail.com>
2025-08-19 10:45:02 -0700

    Update ID when received (#238)
    - update ID when received and updated regexes in validation

--- pages/checkin.js
+++ pages/checkin.js
@@ -134,8 +134,7 @@ class Checkin extends React.Component {
 
   validateCalId = (calIdValue) => {
     const regexes = { 
-      // old: 'isStudent': /^(30\d{8}|[1278]\d{7})$/,
-      'isStudent': /^(\d{8}|\d{10})$/,
+      'isStudent': /^(30\d{8}|[1278]\d{7})$/,
       'isEmployeeOrAffiliate': /^(01\d{6}|10\d{6})$/,
       'isValidEncrypted': /^810:\d{8}$/,
       'isCommunity': /^1$/
@@ -156,12 +155,18 @@ class Checkin extends React.Component {
     return false;
   }
    
+  updateID = (id) => {
+      if (['3', '4', '5', '6'].includes(id[0])) {
+        return '30' + id;
+    } return id; };
+
   handleScanSubmit = async (e) => {
     var fieldset = document.getElementById("calIDFieldset")
     fieldset.disabled = true
     e.preventDefault();
 
-    const { value: calIdValue } = e.target.calID;
+    var { value: calIdValue } = e.target.calID;
+    var calIdValue = this.updateID(calIdValue);
     if (!this.validateCalId(calIdValue)) {
       return
     }
